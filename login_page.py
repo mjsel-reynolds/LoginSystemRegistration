50ec8f7649145465023b5a9d668f92aa851a49d3
maria <mjsel.reynolds@ufl.edu>
2024-07-03 20:16:38 -0400

    Updates: - Updated login page to include user verification GUI For Future: - Fix functionality to properly check if code entered by user is corrected after button is pressed - Resend code functionality

--- login_page.py
+++ login_page.py
@@ -1,7 +1,6 @@
 from tkinter import *#import all classes, functions, etc. from tkinter module
 from tkinter import messagebox
 import pymysql
-import time
 import pyotp
 import smtplib
 from email.message import EmailMessage
@@ -63,18 +62,13 @@ def forgot_pw():
 
 
 def user_verification():
-    # Generate code to send to user
     # Generate Verification Code
     key = pyotp.random_base32()
     totp = pyotp.TOTP(key)
     print(totp.now())
     verify_code = totp.now()  # Makes key based on current time, regenerates every 30 secs
 
-    # Verify code
-    #input_code = input('Enter 2FA Code:')
-    #print(totp.verify(input_code))
-
-    # Check database for email matching username and send verification code
+    # Check database for an email matching username and send verification code
     # Connect to database with userdata
     try:
         mydata = pymysql.connect(host='localhost', user='root', password='Classynotsassy1!')
@@ -106,8 +100,50 @@ def user_verification():
 
     mycursor.close()
     mydata.close()
+
+    # Generate User Verification GUI
     login_root.destroy()
-    #import user_verification
+
+    root = Tk()
+
+    # TITLE SCREEN
+    root.title('User Verification')
+    verify_bg = PhotoImage(file='user_verify.png')
+    verify_bgLabel = Label(root, image=verify_bg)
+    verify_bgLabel.place(x=0, y=0)
+    root.geometry('1174x660+50+50')
+    root.resizable(False, False)
+
+    header = Label(root, text='User Verification', font=('Sanchez', 36), bg='white', fg='#595651')
+    header.place(x=425, y=145)
+
+    # Verification Code Interface
+    code = Label(root, text='Verification Code', font=('Sanchez', 11), bg='#F8F8F8', fg='#595651')
+    code.place(x=455, y=230)
+    codeEntry = Entry(root, width=15, font=('Source Serif Pro', 26), bd=0, fg='#595651', bg='#F8F8F8')
+    codeEntry.place(x=455, y=260)
+
+    submit_code = Button(root, text='SUBMIT', bd=0, bg='#738664', activebackground='#738664', cursor='hand2',
+                         font=('Open Sauce', 19), fg='white', activeforeground='white', width=20, height=2)
+    submit_code.place(x=448, y=400)
+    resend_code = Button(root, text='RESEND CODE', bd=0, bg='#738664', activebackground='#738664', cursor='hand2',
+                         font=('Open Sauce', 15), fg='white', activeforeground='white', width=21, height=1)
+    resend_code.place(x=482, y=500)
+
+    messagebox.showinfo('User Verification', 'A Verification Code has been sent to your email.\n'
+                                             'Enter the code within 30 seconds to access your account.')
+
+    # Verify Code
+    # input_code = input('Enter 2FA Code:')
+    # print(totp.verify(input_code))
+    if totp.verify(codeEntry.get()):
+        messagebox.showinfo('Success', 'User Verified\nAccount Access Granted.')
+    else:
+        messagebox.showerror('Error', 'Code Does Not Match')
+
+    # Resend Code If Button Pressed
+
+    root.mainloop()
 
 
 def login_user():
